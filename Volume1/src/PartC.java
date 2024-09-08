a4114eb013dfb91f1903e957860699d2a82e582a
Aliboop <cole24793@gmail.com>
2024-09-08 12:12:40 -0400

    Added Day 3 Homework Part 3

---
+++ Volume1/src/PartC.java
@@ -0,0 +1,34 @@
+public class PartC {
+    // one
+    final int DAYS_PER_WEEK = 7;
+    int daysUntilSemesterEnds = 26;
+    final double CENTIMETERS_PER_INCH = 2.54;
+    double tallestPersonInCentimeters = 1000.5; //she's very very tall
+    
+    // two
+    /*
+     * Value after statement 1: 1
+     * Value after statement 2: -1
+     * Value after statement 3: 0
+     */
+
+    // three
+    double distanceInitial = 0.1;
+    double velocityInitial = 2.4;
+    double time = 1.6;
+    double acceleration = 0.6;
+
+    double distance = distanceInitial + velocityInitial * time + 0.5*acceleration*Math.pow(time, 2);
+
+    // four
+    /*
+     * a: 1 + 8 = 9
+     * b: 1 + 0 = 1
+     * c: 25 / 2 = 12
+     * d: 25 / 2.0 = 12.5
+     * e: 25 * 0.5 = 12
+     * f: 25 * 0.5 = 13
+     */
+
+
+}
